ece71d3b29341e131cb1a1f4dabd59eff84ad809
usamashafiq <46091325+usamashafiq@users.noreply.github.com>
2021-01-14 20:57:49 +0500

    Update First.py

--- First.py
+++ First.py
@@ -1,78 +1,28 @@
-def getdate():
-    import datetime
-    return datetime.datetime.now()
-
-
-print("   HEALTH MANAGEMENT SYSTEM")
-
-print("     NUMBER OF MEMBER")
-print("        Rohan  for 1")
-print("        Hammad for 2")
-
-x = int(input())
-
-if x == 1:
-    print("Hi Rohan")
-    print("Choose the file : READ OR WRITE MODE WRITE FOR W AND READ FOR R")
-    c=input("Enter the character")
-    if c=='w':
-
-        print("FOR FOOD SELECT  1", "FOR Exercise SELECT 2")
-        y = int(input())
-        if y == 1:
-
-            f = open("rohanf.txt", "w")
-            F = input("Enter the FOOD : ")
-            f.write(str(getdate()))
-            f.write(F)
-            f.close()
-        else:
-            f = open("rohane.txt", "w")
-            F = input("Enter the Exercise : ")
-            f.write(str(getdate()))
-            f.write(F)
-            f.close()
+import random
+cout,coutp=0,0
+print("**sank water gun game**")
+list=['snak','water','gun']
+i=0
+while(i<4):
+    i+=1
+    x=random.choice(list)
+    y=input("Enter the choice")
+    print(x)
+    if x==y:
+        print("tie")
+    elif x=='snak' and y=='water':
+        cout=cout+1
+                    #print("you loss")
+    elif x=='water'  and y == 'gun':
+        cout = cout+1
+                   # print("you loss")
+    elif x=='gun' and y=='snak':
+        cout = cout + 1
+                  #  print("you loss")
     else:
-        print("FOR FOOD SELECT  1", "FOR Exercise SELECT 2")
-        y = int(input())
-        if y == 1:
-
-            f = open("rohanf.txt", "r")
-            print(f.read())
-            f.close()
-        else:
-            f = open("rohane.txt", "r")
-            print(f.read())
-            f.close()
-elif x==2:
-    print("Choose the file : ")
-
-    print("FOR FOOD SELECT  1", "FOR Exercise SELECT 2")
-    y = int(input())
-    if y == 1:
-
-        f = open("Hammadf.txt", "w")
-        F = input("Enter the FOOD : ")
-        f.write(str(getdate()))
-        f.write(F)
-        f.close()
-    else:
-        f = open("Hammade.txt", "w")
-        F = input("Enter the Exercise : ")
-        f.write(str(getdate()))
-        f.write(F)
-        f.close()
-else:
-    print("FOR FOOD SELECT  1", "FOR Exercise SELECT 2")
-    y = int(input())
-    if y == 1:
-
-        f = open("rohanf.txt", "r")
-        print(f.read())
-        f.close()
-    else:
-        f = open("rohane.txt", "r")
-        print(f.read())
-        f.close()
+        coutp+=1
 
 
+
+print("Number try PC: ",cout)
+print("Number try player : ",coutp)
